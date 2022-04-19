52eafd2fa63111874da6c7ac888a375fdf837ed7
Nesterov Victor <vector.nesterov@gmail.com>
2022-04-19 08:47:54 +0300

    Fix addresses for GAME_13X_1

--- pkodev.mod.reward.client/address.h
+++ pkodev.mod.reward.client/address.h
@@ -101,42 +101,42 @@ namespace pkodev
 			const unsigned int CUIInterface = 0x0066A288;
 
 			// CForm* CUIInterface::_FindForm(const char * frmName)
-			const unsigned int CUIInterface___FindForm = 0x0049E390;
+			const unsigned int CUIInterface___FindForm = 0x0049E4A0;
 
 			// void CForm::Show()
-			const unsigned int CForm__Show = 0x00493FD0;
+			const unsigned int CForm__Show = 0x004940E0;
 
 			// void CForm::ShowModal()
-			const unsigned int CForm__ShowModal = 0x00493F50;
+			const unsigned int CForm__ShowModal = 0x00494060;
 
 			// // void CForm::Hide()
-			const unsigned int CForm__Hide = 0x00493430;
+			const unsigned int CForm__Hide = 0x00493540;
 
 			// CCompent* CForm::Find( const char* str )
-			const unsigned int CForm__Find = 0x00494040;
+			const unsigned int CForm__Find = 0x00494150;
 
 			// inline CItemRecord* GetItemRecordInfo( int nTypeID )
 			const unsigned int GetItemRecordInfo = 0x0040CE90;
 
 			// CItemCommand::CItemCommand(CItemRecord* pItem)
-			const unsigned int CItemCommand__CItemCommand = 0x004898E0;
+			const unsigned int CItemCommand__CItemCommand = 0x004899A0;
 
 			// void CItemCommand::SetTotalNum(int num)
-			const unsigned int CItemCommand__SetTotalNum = 0x00488280;
+			const unsigned int CItemCommand__SetTotalNum = 0x00488340;
 
 			// [7E 08] -> [90 90]
 			// JLE SHORT 004883FC -> NOP NOP
-			const unsigned int CItemCommand__SetTotalNum__Patch = 0x00488292;
+			const unsigned int CItemCommand__SetTotalNum__Patch = 0x00488352;
 
 			// inline void CItemCommand::SetIsSolid( bool v )      
-			const unsigned int CItemCommand__SetIsSolid = 0x00488B80;
+			const unsigned int CItemCommand__SetIsSolid = 0x00488C40;
 
 			// void CItemCommand::Render(int x, int y)
-			const unsigned int CItemCommand__Render = 0x00488F00;
+			const unsigned int CItemCommand__Render = 0x00488FC0;
 
 			// [66 3D 01 00] -> [66 83 F8 00]
 			// CMP AX, 1 -> CMP AX,0
-			const unsigned int CItemCommand__Render__Patch = 0x00488F43;
+			const unsigned int CItemCommand__Render__Patch = 0x00489003;
 
 			// void COneCommand::AddCommand( CCommandObj* p )  
 			const unsigned int COneCommand__AddCommand = 0x00421AC0;
@@ -148,40 +148,40 @@ namespace pkodev
 			const unsigned int CGuiPic = 0x00654000;
 
 			// CGuiPic::CGuiPic( CGuiData* pParent, unsigned int max )
-			const unsigned int CGuiPic__CGuiPic = 0x0049D640;
+			const unsigned int CGuiPic__CGuiPic = 0x0049D750;
 
 			// bool	CGuiPic::LoadImage( const char * file, int w, int h, int frame=0, int tx=0, int ty=0, float scalex=0.0, float scaley=0.0 )
-			const unsigned int CGuiPic__LoadImage = 0x0049FDC0;
+			const unsigned int CGuiPic__LoadImage = 0x0049FED0;
 
 			// inline void CGuiPic::Render( int x, int y )	
-			const unsigned int CGuiPic__Render = 0x0049ED60;
+			const unsigned int CGuiPic__Render = 0x0049EE70;
 
 			// NetIF
-			const unsigned int NetIF = 0x006C2508;
+			const unsigned int NetIF = 0x006C25F8;
 
 			// BOOL NetIF::HandlePacketMessage(DataSocket *datasock,LPRPACKET pk)
-			const unsigned int NetIF__HandlePacketMessage = 0x00516D30;
+			const unsigned int NetIF__HandlePacketMessage = 0x00516E40;
 
 			// uShort RPacket::ReadCmd()
-			const unsigned int RPacket__ReadCmd = 0x0054F7C0;
+			const unsigned int RPacket__ReadCmd = 0x0054F900;
 
 			// uLong RPacket::ReadLong()
-			const unsigned int RPacket__ReadLong = 0x0054FA80;
+			const unsigned int RPacket__ReadLong = 0x0054FBC0;
 
 			// void *__cdecl operator new(size_t)
-			const unsigned int operator__new = 0x0059C988;
+			const unsigned int operator__new = 0x0059CAC8;
 
 			// TcpCommApp
-			const unsigned int TcpCommApp = 0x006C2508;
+			const unsigned int TcpCommApp = 0x006C25F8;
 
 			// WPacket TcpCommApp::GetWPacket()const
-			const unsigned int TcpCommApp__GetWPacket = 0x005515C0;
+			const unsigned int TcpCommApp__GetWPacket = 0x00551700;
 
 			// bool WPacket::WriteCmd(uShort cmd)
-			const unsigned int WPacket__WriteCmd = 0x0054EA40;
+			const unsigned int WPacket__WriteCmd = 0x0054EB80;
 
 			// void NetIF::SendPacketMessage(LPWPACKET pk)
-			const unsigned int NetIF__SendPacketMessage = 0x005193E0;
+			const unsigned int NetIF__SendPacketMessage = 0x005194F0;
 		}
 
 		// Game.exe 2 (1252912474)
